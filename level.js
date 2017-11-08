5c08463aa850d81ef284bef2e8b4a706f281d2b1
Jaime Ventor <jventor@officepoint.es>
2017-11-08 23:09:53 +0000

    Terminada seccion C

--- level.js
+++ level.js
@@ -1,5 +1,5 @@
 const ACTORS = {
-
+    'o': Coin
 };
 
 function Level (plan){
@@ -7,14 +7,17 @@ function Level (plan){
     this.height = plan.length;
 
     this.grid = [];
+    this.actors = [];
 
     for (let y = 0; y < this.height; y++){
         let line = plan[y];
         let gridLine = [];
         for (let x = 0; x < this.width; x++){
             let character = line[x];
-            let characterType = null
-;
+            let characterType = null;
+
+            let Actor = ACTORS[character];
+            if (Actor) this.actors.push(new Actor(new Vector(x,y), character));
             if (character === 'x') characterType = 'wall'
             else if (character === '!') characterType = 'lava';
 
